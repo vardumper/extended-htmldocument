779eef0aff2398fed9d38bd5bc57c964d266f509
Erik Pöhler <admin@teuton.mx>
2025-11-26 07:27:47 +0100

    chore: added html logo

--- templates/storybook/.storybook/theme.ts
+++ templates/storybook/.storybook/theme.ts
@@ -1,13 +1,11 @@
 import { create } from 'storybook/theming';
 
+import logo from '../static/html5-logo.svg';
+
 export default create({
   base: "light",
-  brandTitle: "PicoCSS",
-  brandUrl: "https://picocss.com",
-  brandImage: `https://picocss.com/build/_assets/pico-logo-light-OKNA7EES.svg`,
+  brandTitle: "Extended HTMLDocument",
+  brandUrl: "https://vardumper.github.io/extended-htmldocument/",
+  brandImage: logo,
   brandTarget: "_blank",
-
-  // You can control image size with these properties (in pixels)
-  // Note: These don't directly control image size, but control the sidebar width
-  // To control image size, use a preview-head.html file with custom CSS
 });
